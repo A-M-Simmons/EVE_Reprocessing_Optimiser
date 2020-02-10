7a48e8b8fecf203451a34a925ca7f1f683c0a3b9
Alex <Alex@DESKTOP-8U3K6KS>
2020-02-10 13:49:54 +1000

    removed main

--- main.py
+++
@@ -1,9 +0,0 @@
-
-
-from models import Reprocessing
-from solver import Ore_Reprocessing_Solver
-
-r = Ore_Reprocessing_Solver()
-r.set_reprocessing(0.874)
-r.set_mineral_constraints([2555556, 666667, 194444, 22222, 17222, 1778, 8444, 0])
-r.solve()
