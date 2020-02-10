e9b0ad71d4e3c62adec29c552d32e44423a3034b
Alex <Alex@DESKTOP-8U3K6KS>
2020-02-10 13:47:15 +1000

    changed name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Reprocessing-Solver-Alex-M-Simmons", # Replace with your own username
+    name="EVE_Reprocessing_Solver", # Replace with your own username
     version="0.0.1",
     author="Alex Simmons",
     author_email="author@example.com",
